01e0e76c1421f6ecbaa81e89b357370f9d8d9dda
Acekhing <charlesannorblay@gmail.com>
2022-11-26 09:53:34 +0000

    Added DbContext

--- EF6DataValidation/Program.cs
+++ EF6DataValidation/Program.cs
@@ -1,6 +1,13 @@
+using EF6DataValidation.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseInMemoryDatabase("CheapPhones");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
