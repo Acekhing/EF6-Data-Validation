4b4408e68ea8b160f013f42a9411483597cc3d8b
Acekhing <charlesannorblay@gmail.com>
2022-11-26 11:21:51 +0000

    Initial Project setup

--- EF6DataValidation/Models/Phone.cs
+++ EF6DataValidation/Models/Phone.cs
@@ -4,19 +4,9 @@ namespace EF6DataValidation.Models
 {
     public class Phone
     {
-        [Required]
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(50, ErrorMessage ="Phone name cannot exceed 50 characters")]
         public string Name { get; set; }
-
-        [Required]
-        [Range(minimum: 10.0, maximum:99.9, ErrorMessage = "Product price must be between $10.0 - $99.9")]
         public decimal Price { get; set; }
-
-        [Required]
-        [RegularExpression(pattern: @"\bApple|Google|Samsung\b", ErrorMessage ="We accept only Apple, Google and Samsung phone")]
         public string Brand { get; set; }
         public string Version { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
